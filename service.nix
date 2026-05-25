d2c26cf9b8dd3695ed076fc15e6d2115e0146d61
Jordan Dominion <Cyberboss@users.noreply.github.com>
2026-05-24 21:29:38 -0400

    Try

--- service.nix
+++ service.nix
@@ -36,9 +36,7 @@ let
     ${pkgs.systemd}/bin/systemd-notify --status="Patching binaries..."
     for dir in ${headless-directory}/runtimes/*/; do
       echo "Entering $dir"
-      subdir_glob="''${dir}native/*.so";
-      echo "Globbing $subdir_glob"
-      for file in $subdir_glob; do
+      for file in ''${dir}native/*.so; do
         echo "Patching $file"
         ${pkgs.patchelf}/bin/patchelf --set-rpath "${pkgs.libpng}/lib:${pkgs.zlib}/lib:${pkgs.bzip2}/lib" $file
       done
